3300d68e05194220c7f1d83742dbdefd3db7a67b
scarh201 <sinan.kekic@gmail.com>
2023-10-01 20:12:32 -0500

    i hope this works

---
+++ ros2_ws/build/turtlebot3/colcon_command_prefix_build.sh
@@ -0,0 +1,7 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/user/ros2_ws/install/turtlebot3_description/share/turtlebot3_description/package.sh"
+. "/home/user/ros2_ws/install/turtlebot3_msgs/share/turtlebot3_msgs/package.sh"
+. "/home/user/ros2_ws/install/turtlebot3_navigation2/share/turtlebot3_navigation2/package.sh"
+. "/home/user/ros2_ws/install/turtlebot3_teleop/share/turtlebot3_teleop/package.sh"
+. "/home/user/ros2_ws/install/turtlebot3_bringup/share/turtlebot3_bringup/package.sh"
+. "/home/user/ros2_ws/install/turtlebot3_example/share/turtlebot3_example/package.sh"
---
+++ ros2_ws/install/project_path_planning/share/project_path_planning/launch/nav_to_pose_action_client_launch_file.launch.py
@@ -0,0 +1,10 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='path_planner_server',
+            executable='nav_to_pose_action_client',
+            output='screen'),
+    ])
